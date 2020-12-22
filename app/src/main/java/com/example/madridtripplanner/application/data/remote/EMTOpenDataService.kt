c0154b1decb33253214d42bcf57c69d3685bbd46
jrg89 <jorgemartinalvarado@gmail.com>
2020-12-22 21:03:25 +0100

    Create room database and retrofit client/service

---
+++ app/src/main/java/com/example/madridtripplanner/application/data/remote/EMTOpenDataService.kt
@@ -0,0 +1,26 @@
+package com.example.madridtripplanner.application.data.remote
+
+import com.example.madridtripplanner.application.data.remote.model.ApiInfoResult
+import com.example.madridtripplanner.application.data.remote.model.AppInfoResult
+import com.example.madridtripplanner.application.data.remote.model.BusLinesResult
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface EMTOpenDataService {
+
+    @GET("hello/")
+    suspend fun hello(): ApiInfoResult
+
+    @GET("mobilitylabs/user/login/")
+    suspend fun login(
+        @Header("X-ClientId") clientID: String,
+        @Header("passKey") passKey: String
+    ): AppInfoResult
+
+    @GET("transport/busemtmad/lines/info/{dateRef}/")
+    suspend fun getLines(
+        @Header("accessToken") accessToken: String,
+        @Query("dateref") dateRef: String
+    ): BusLinesResult
+}
